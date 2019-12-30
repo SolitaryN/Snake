caaaf438f7b6f283d787b35217f9e64e4b1416ea
solitary <2331017424@qq.com>
2019-12-30 11:32:10 +0800

    first commit

--- Snake/Snake.h
+++ Snake/Snake.h
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define TRUE 1
+#define FLASE 0
 
 typedef struct StackNode{
 	struct Node * Next;                                                                                                                                                                                                                                                                                                              
 	char data;
-}StackNode;//此处定义了链栈的节点 
+}StackNode;					//此处定义了链栈的节点 
 
 typedef struct LinkNode{
-	int weight;
+	int weight;				//权值 
 	char data;
 	struct LinkNode * Next;
-} LinkNode;//此处定义了单链表的节点，用来保存出现的都有哪些字符，还有字符出现的次数 
+} LinkNode;					//此处定义了单链表的节点，用来保存出现的都有哪些字符，还有字符出现的次数 
 
 //创建Top和Head，它们都带头结点 
 StackNode Top;
@@ -19,7 +21,7 @@ LinkNode Head;
 Top->Next = NULL;
 Head->Next = NULL; 
 
-void InsertLink(LinkNode * Head, char data)//此处的单链表插入用的是头插法 
+void InsertLink(LinkNode * Head, char data)		//此处的单链表插入用的是头插法 
 {
 	LinkNode * temp;
 	temp = (LinkNode *) malloc (LinkNode);
@@ -33,7 +35,7 @@ void InsertLink(LinkNode * Head, char data)//此处的单链表插入用的是头插法
 	Head->Next = temp;
 } 
 
-void Push(StackNode * top, char data)//入栈函数, top为栈顶 
+void Push(StackNode * top, char data)			//入栈函数, top为栈顶 
 {
 	StackNode * temp;
 	temp = (StackNode *) malloc (StackNode);
@@ -47,11 +49,31 @@ void Push(StackNode * top, char data)//入栈函数, top为栈顶
 	top->Next = temp;
 }
 
+int JudegeAppear(char data)//判断字符是否在这个链表中出现过，如果没出现过，返回TRUE保存
+{
+	LinkNode * temp;
+	temp = Head->Next;
+	int j = 0;			//此处的j用来判断是否将要入栈的字符为第一次出现的字符
+	while(temp != NULL)
+	{	
+		if(data == temp->data)
+		{
+			temp->weight++;
+			j = 1;
+			return TRUE;//出现过 
+		}
+		temp =  temp->Next;
+	}
+	if(j == 0)
+		InsertLink(Head, data);	//如果新进入的字符是新字符，就单链表中创建一个新的节点用来保存，未出现过 
+}
+
 void Statistic(char * str)
 {
 	int i = 0;
 	while(*(str + i) != '\0')
 	{
+			 
 		
 	}
 }
